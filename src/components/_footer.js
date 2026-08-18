73512860ad098da30985b2feb9d34f695d68271a
Eric Gideon <eric.gideon@gmail.com>
2026-08-18 16:23:48 +0300

    attempt to blow up old inline styles

--- src/components/_footer.js
+++ src/components/_footer.js
@@ -4,13 +4,7 @@ import * as styles from "../components/_footer.module.css"
 
 const Footer = () => {
 	return (
-		<footer
-			style={{
-				// marginTop: `var(--space-5)`,
-				// fontSize: `var(--font-sm)`,
-			}}
-			className={styles.footer}
-		>
+		<footer className={styles.footer}>
 			<div className={styles.aboutSection}>
 				<p className={styles.about} lang="en">
 					Founded in 2021, <strong>The 5th Cat aims to help street cats in Greece</strong>, particularly in Syros and Athens. This is accomplished with the <strong>help of a local and international solidarity network</strong> that organizes trap, neuter, vaccinate and release (TNVR) programs, <strong>rescues and adoptions</strong>.
