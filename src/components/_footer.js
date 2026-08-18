be20ad3735cb2c7d11372433e1003cb98697c0a8
Eric Gideon <eric.gideon@gmail.com>
2026-08-18 14:52:22 +0300

    footer layout, ish

--- src/components/_footer.js
+++ src/components/_footer.js
@@ -11,30 +11,33 @@ const Footer = () => {
 			}}
 			className={styles.footer}
 		>
-			<p className={styles.about} lang="en">
-				Founded in 2021, The 5th Cat aims to help street cats in Greece, particularly in Syros and Athens. This is accomplished with the help of a local and international solidarity network that organizes trap, neuter, vaccinate and release (TNVR) programs, rescues and adoptions.
-			</p>
-			<StaticImage
-				className={styles.footerMap}
-				src="../images/map-syros.png"
-				loading="lazy"
-				placeholder="none"
-				width={200}
-				quality={25}
-				formats={["auto", "jpg", "webp", "png"]}
-				alt=""
-				/>
-			<StaticImage
-				className={styles.footerCat}
-				src="../images/art-cat-side@2x.png"
-				loading="lazy"
-				placeholder="none"
-				width={300}
-				quality={95}
-				formats={["auto", "webp", "png"]}
-				alt=""
-				/>
-
+			<div className={styles.aboutSection}>
+				<p className={styles.about} lang="en">
+					Founded in 2021, <strong>The 5th Cat aims to help street cats in Greece</strong>, particularly in Syros and Athens. This is accomplished with the <strong>help of a local and international solidarity network</strong> that organizes trap, neuter, vaccinate and release (TNVR) programs, <strong>rescues and adoptions</strong>.
+				</p>
+				<div className={styles.mapCat}>
+					<StaticImage
+						className={styles.footerMap}
+						src="../images/map-syros.png"
+						loading="lazy"
+						placeholder="none"
+						width={200}
+						quality={25}
+						formats={["auto", "jpg", "webp", "png"]}
+						alt=""
+						/>
+					<StaticImage
+						className={styles.footerCat}
+						src="../images/art-cat-side@2x.png"
+						loading="lazy"
+						placeholder="none"
+						width={300}
+						quality={95}
+						formats={["auto", "webp", "png"]}
+						alt=""
+						/>
+					</div>
+			</div>
 			<p className={styles.copyright}>© Le 5ème Chat / The 5th Cat {new Date().getFullYear()}</p>
 			<div className={styles.charityInfo}>
 				<p lang="fr">ASSOCIATION À BUT NON LUCRATIF Déclaré en France — n°W883005877</p>
